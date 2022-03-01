0c2f640fb5919fb0f45cfcecd7eb478926ec6e89
Nae Cosmin <cosminnae00@yahoo.com>
2022-03-01 20:36:07 +0200

    game done

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
-let cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll('.memory-card');
+
+let firstCard;
+let secondCard;
+cardFliped = false;
+let lockBoard = false;
+
+function flipCard(e) {
+  if (lockBoard) return;
+  if (firstCard === this) return;
+  this.classList.add('flip');
+
+  if (!cardFliped) {
+    // first click
+    cardFliped = true;
+    firstCard = this;
+  } else {
+    // second click
+    cardFliped = false;
+    secondCard = this;
+    // If the cards match run this
+    if (firstCard.dataset.item === secondCard.dataset.item) {
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+    }
+    // if the cards dont match run this
+    else {
+      lockBoard = true;
+
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        lockBoard = false;
+      }, 900);
+    }
+  }
+}
 
 cards.forEach((card) => card.addEventListener('click', flipCard));
 
-function flipCard(e) {
-  this.classList.toggle('flip');
-}
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomNum = Math.floor(Math.random() * 12);
+    card.style.order = randomNum;
+  });
+})();
